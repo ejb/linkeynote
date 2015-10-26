4ecafa06b8b8e5b0be48f33a14130ed54215b993
ejb <mail@elliotbentley.com>
2015-10-26 22:47:15 +0000

    Preload next iframe

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 var app = require('app');  // Module to control application life.
-var fs = require('fs');
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var globalShortcut = require('global-shortcut');
-
-var links = fs.readFileSync('links.txt', 'utf8').split('\n');
-var linkIndex = 0;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -23,12 +19,10 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, "node-integration": false});
+  mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadUrl( 'file://' + __dirname + '/viewer.html' );      
 
-  goToLink(0);
-
-  mainWindow.setAspectRatio(9/16);
+  // mainWindow.setAspectRatio(9/16);
   // mainWindow.setFullScreen(true);
   var webContents = mainWindow.webContents;
   webContents.on('dom-ready', function(){
@@ -43,26 +37,16 @@ app.on('ready', function() {
     mainWindow = null;
   });
   
-  globalShortcut.register('left', function() {
-    goToLink( linkIndex-1 );
-  });
-  globalShortcut.register('right', function() {
-    goToLink( linkIndex+1 );
-  });
+  globalShortcut.register('left', prev);
+  globalShortcut.register('right', next );
   
 });
 
-function goToLink(index) {
-  if (links[linkIndex]) {
-    if (links[linkIndex].substring(0,4) === 'http') {
-      mainWindow.webContents.executeJavaScript( 'goToUrl("'+links[linkIndex]+'");' )
-      // mainWindow.loadUrl( 'file://' + __dirname + '/viewer.html#'+links[linkIndex] );
-    } else {
-      
-    }
-    linkIndex = index;
-  }
+function next(){
+    mainWindow.webContents.executeJavaScript( 'next();' );
 }
-
+function prev(){
+    mainWindow.webContents.executeJavaScript( 'prev();' );
+}
 
 
